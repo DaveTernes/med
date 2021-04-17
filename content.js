dd9b4fcfa0d97638f2798a98c159954f3cb6e007
Dave <dave@walnutshade.com>
2021-04-17 14:48:45 -0500

    hid control

--- content.js
+++ content.js
@@ -1,12 +1,14 @@
 
 //create input and anchor elements
 var element = document.getElementById("main");
-element.innerHTML = "<div><input type='text' id='address' value=''><a id='myLink' href=''></a></div>";
+element.innerHTML = "<div><input type='text' id='address' value=''><a id='myLink' href=''>Redirecting...</a></div>";
+
 
 //fill input from paste
 var myInput = document.getElementById('address');
 myInput.select();
 document.execCommand('paste');
+myInput.style.visibility = "hidden";
 
 //fill link from input
 var myLink = document.getElementById('myLink');
